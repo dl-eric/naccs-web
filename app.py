9b02b5d732261f4472d3f886c46952081945d2c4
Eric <ericdl712@gmail.com>
2019-02-10 17:52:55 -0600

    Add preliminary front end ui elements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from auth import auth_page
 
 app = Flask(__name__)
@@ -7,4 +7,4 @@ app.register_blueprint(auth_page)
 
 @app.route('/')
 def index():
-    return "Hello Azure!"
+    return render_template("index.html")
