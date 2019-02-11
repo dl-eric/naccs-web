3a3f627913175c069640470dfdc7511a8bdce311
Eric <ericdl712@gmail.com>
2019-02-11 04:14:17 -0600

    Change secret key to production key

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from auth import auth_page
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'such a secret wow'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
 app.register_blueprint(auth_page)
 
 @app.route('/')
