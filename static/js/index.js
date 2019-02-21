4dadb414cc67794cb99252166da8e2081482d2ae
Eric <ericdl712@gmail.com>
2019-02-20 22:09:55 -0600

    Add responsiveness

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,32 @@ $(document).ready(function(){
     $('.matches').slick({
       infinite: false,
       slidesToShow: 5,
+      responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 4,
+            }
+        },
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+      ]
     });
     $('.matches').css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1}, 600);
 });
