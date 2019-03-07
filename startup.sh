80df9cb3199175e3dc7812bcbf65450960d65c82
Eric Lee <ericdl2@illinois.edu>
2019-03-06 22:06:03 -0600

    Ignore failure on removing naccs image

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 docker build -t naccs_image .
-docker stop naccs
-docker rm naccs
+docker stop naccs || true
+docker rm naccs || true
 docker run --env-file ./env.list -d --name naccs -p 80:80 naccs_image
