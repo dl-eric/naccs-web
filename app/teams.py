1497d9134a6e1ba763200deac40fb4b10b8e1d16
Isaias Cuevas <icuevas2757@ung.edu>
2019-05-10 21:37:43 -0400

    Pay Button/Leave Button Update
    Leave button will only show if you are on YOUR team page. Pay button will only show if user is in the database.

--- app/teams.py
+++ app/teams.py
@@ -22,7 +22,7 @@ def login_required(view):
 
 
 @teams_page.route('/' , methods=['get', 'post'])
-def teams(): 
+def teams():
     # Fetches the currently registered teams.
     teams = Teams.query.order_by(Teams.team_id.desc()).all()
     player_check = Players.query.filter(Players.name == session.get('username')).first()
@@ -35,10 +35,11 @@ def rules():
 
 
 @teams_page.route('/<name>', methods=['post', 'get'])
-def team(name): 
+def team(name):
     # Check if team exists
     team = Teams.query.filter(Teams.name == name).first()
     players = db.engine.execute('SELECT * FROM `players` WHERE players.team_id = '+ str(team.team_id) +'')
+    p = Players.query.filter(Players.name == session.get('username')).first()
     if name == None:
         abort(404)
     form = JoinTeam()
@@ -49,32 +50,32 @@ def team(name):
                 already_on_team = Players.query.filter(Players.name == session.get('username')).first()
                 already_leader = Players.query.filter(Teams.leader == session.get('username')).count()
                 if already_on_team.team_id != 0:
-                    flash('You are already on a team.', 'error')  
+                    flash('You are already on a team.', 'error')
                     return redirect(url_for('teams.teams'))
                 if already_leader > 0:
-                    flash('You are already the leader of a team.', 'error')  
+                    flash('You are already the leader of a team.', 'error')
                     return redirect(url_for('teams.teams'))
-                #if player isnt in players table it will create a new row for the player       
+                #if player isnt in players table it will create a new row for the player
                 elif already_on_team == None:
                     new_post = Players(session.get('username'), 'null', team.team_id, 'false')
                     db.session.add(new_post)
                     db.session.commit()
-                    flash('You have joined the team', 'success')  
+                    flash('You have joined the team', 'success')
                     return redirect(url_for('teams.teams'))
                 #if player is in the table then it will just update their team id to the team they joined
                 else:
                     already_on_team.team_id = team.team_id
                     db.session.commit()
-                    flash('You have joined the team', 'success')  
+                    flash('You have joined the team', 'success')
                     return redirect(url_for('teams.teams'))
             else:
-                flash('The password you entered is incorrect!', 'error') 
+                flash('The password you entered is incorrect!', 'error')
                 return redirect(url_for('teams.teams'))
     lform = LeaveTeamForm()
     if lform.validate_on_submit():
         return redirect(url_for('teams.leaveconfirm'))
 
-    return render_template("teams/team.html", username=session.get('username'), team=team, players=players, form=form, lform=lform)  
+    return render_template("teams/team.html", username=session.get('username'), team=team, players=players, form=form, lform=lform, p=p)
 
 
 @teams_page.route('/createteam', methods=['post', 'get'])
@@ -101,17 +102,17 @@ def create():
             db.session.commit()
             team1 = Teams.query.filter(Teams.name == form.name.data).first()
             new_player = Players(session.get('username'), form.school.data, team1.team_id, 'false')
-            db.session.add(new_player) 
+            db.session.add(new_player)
             db.session.commit()
             flash('Your team has been successfully created!', 'success')
             return redirect(url_for('teams.teams'))
         else:
-            players = Players.query.filter(Players.name == session.get('username')).first()           
+            players = Players.query.filter(Players.name == session.get('username')).first()
             enc_pass = bcrypt_sha256.hash(form.password.data)
             new_post = Teams(form.school.data, form.name.data, form.teamtype.data, session.get('username'), enc_pass)
             db.session.add(new_post)
             db.session.commit()
-            tid = Teams.query.filter(Teams.name == form.name.data).first() 
+            tid = Teams.query.filter(Teams.name == form.name.data).first()
             players.team_id = tid.team_id
             db.session.commit()
             flash('Your team has been successfully created!', 'success')
@@ -131,7 +132,7 @@ def leaveconfirm():
             leader = Teams.query.filter(Teams.leader == session.get('username')).first()
             if user == None:
                 flash('You are not currently on a team.', 'error')
-                return redirect(url_for('teams.teams')) 
+                return redirect(url_for('teams.teams'))
             # if statement checking whether theyre a leader (if theyre leader delete the team theyre a leader of)
             # set the ids of all users that were on the team to null or 0
             elif leader != None:
@@ -146,13 +147,13 @@ def leaveconfirm():
                 db.session.delete(leader)
                 db.session.commit()
                 flash('You have successfully left your team (Your team was deleted!).', 'success')
-                return redirect(url_for('teams.teams')) 
-            else: 
-                #player is not leader and is just leaving his team.   
+                return redirect(url_for('teams.teams'))
+            else:
+                #player is not leader and is just leaving his team.
                 user.team_id = 0
                 db.session.commit()
                 flash('You have successfully left your team.', 'success')
-                return redirect(url_for('teams.teams')) 
+                return redirect(url_for('teams.teams'))
         elif form.accept.data == False:
             flash('You must accept to leave your team.', 'error')
             return redirect(url_for('teams.teams'))
